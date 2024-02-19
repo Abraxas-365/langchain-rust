26ff7cd700b3a68eec864f0ef764407a1d954c1e
Abraxas-365 <luisfmiranda8@gmail.com>
2024-02-18 19:31:32 -0500

    feat(memory): Simple memory

--- src/schemas/memory.rs
+++ src/schemas/memory.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use super::messages::Message;
 
 pub trait BaseMemory: Send + Sync {
@@ -21,5 +23,35 @@ pub trait BaseMemory: Send + Sync {
             .map(|msg| format!("{:?}:{:?}", msg.message_type, msg.content))
             .collect::<Vec<String>>()
             .join("\n")
+    }
+}
+
+pub struct SimpleMemory {
+    messages: Vec<Message>,
+}
+
+impl SimpleMemory {
+    pub fn new() -> Self {
+        Self {
+            messages: Vec::new(),
+        }
+    }
+}
+
+impl Into<Arc<dyn BaseMemory>> for SimpleMemory {
+    fn into(self) -> Arc<dyn BaseMemory> {
+        Arc::new(self)
+    }
+}
+
+impl BaseMemory for SimpleMemory {
+    fn messages(&self) -> Vec<Message> {
+        self.messages.clone()
+    }
+    fn add_message(&mut self, message: Message) {
+        self.messages.push(message);
+    }
+    fn clear(&mut self) {
+        self.messages.clear();
     }
 }
