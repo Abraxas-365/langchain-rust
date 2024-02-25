9f25d9f544582dd7ad8dc3dc98e59198ebf854a6
Abraxas-365 <luisfmiranda8@gmail.com>
2024-02-25 00:26:13 -0500

    fix(textsplitter): adding self

--- src/text_splitter/text_splitter.rs
+++ src/text_splitter/text_splitter.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
 
 pub trait TextSplitter {
-    fn split_text(text: &str) -> Result<Vec<String>, Box<dyn Error>>;
+    fn split_text(&self, text: &str) -> Result<Vec<String>, Box<dyn Error>>;
 }
