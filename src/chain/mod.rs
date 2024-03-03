a2d734c82713c64e3775b0ee759e389107d7d039
Abraxas-365 <luisfmiranda8@gmail.com>
2024-03-03 14:45:24 -0500

    chore: Add sequential module to chain module

--- src/chain/mod.rs
+++ src/chain/mod.rs
@@ -2,9 +2,5 @@ pub mod chain_trait;
 pub mod conversational;
 pub mod llm_chain;
 pub mod options;
+pub mod sequential;
 pub mod sql_datbase;
-
-pub use chain_trait::*;
-pub use conversational::*;
-pub use llm_chain::*;
-pub use sql_datbase::*;
