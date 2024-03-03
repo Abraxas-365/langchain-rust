d1fc5aff3bdc38705d0ea8fa3aabafb01770f80a
Abraxas-365 <luisfmiranda8@gmail.com>
2024-03-03 11:25:49 -0500

    chore: change exporting way

--- src/chain/mod.rs
+++ src/chain/mod.rs
@@ -3,3 +3,8 @@ pub mod conversational;
 pub mod llm_chain;
 pub mod options;
 pub mod sql_datbase;
+
+pub use chain_trait::*;
+pub use conversational::*;
+pub use llm_chain::*;
+pub use sql_datbase::*;
