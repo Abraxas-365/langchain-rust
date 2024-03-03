60691d45d2c9b7e5085a3859dc38ca8ccdec8764
Abraxas-365 <luisfmiranda8@gmail.com>
2024-03-03 14:46:12 -0500

    chore(chain): refactor chain module structure and reorganize exports

--- src/chain/mod.rs
+++ src/chain/mod.rs
@@ -2,5 +2,11 @@ pub mod chain_trait;
 pub mod conversational;
 pub mod llm_chain;
 pub mod options;
-pub mod sequential;
+mod sequential;
 pub mod sql_datbase;
+
+pub use chain_trait::*;
+pub use conversational::*;
+pub use llm_chain::*;
+pub use sequential::*;
+pub use sql_datbase::*;
