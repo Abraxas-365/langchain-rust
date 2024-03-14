e2a70feda69dd6b711b1d417ac615a055f12fd7f
Abraxas-365 <luisfmiranda8@gmail.com>
2024-03-13 22:12:04 -0500

    chore: add debug and clone traits to TextLoader struct

--- src/document_loaders/text_loader/text_loader.rs
+++ src/document_loaders/text_loader/text_loader.rs
@@ -4,6 +4,7 @@ use async_trait::async_trait;
 
 use crate::{document_loaders::Loader, schemas::Document, text_splitter::TextSplitter};
 
+#[derive(Debug, Clone)]
 pub struct TextLoader {
     content: String,
 }
