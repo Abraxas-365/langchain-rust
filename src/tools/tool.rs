8a08a1e3a7c09052c36916f6e7d75274eb891fb9
Abraxas-365 <luisfmiranda8@gmail.com>
2024-03-21 18:12:10 -0500

    chore: Update tool.rs with additional documentation and method implementations
    - Added a method to provide a description of the tool
    - Updated formatting and comments for better readability
    - Implemented methods for parsing input, running the tool, and calling the tool asynchronously
    
    This commit improves the documentation and functionality of the tool.rs file.

--- src/tools/tool.rs
+++ src/tools/tool.rs
@@ -6,11 +6,14 @@ use serde_json::{json, Value};
 
 #[async_trait]
 pub trait Tool: Send + Sync {
+    /// Returns the name of the tool.
     fn name(&self) -> String;
+
+    /// Provides a description of what the tool does and when to use it.
     fn description(&self) -> String;
-    ///This are the parametters for OpenAi like function call.
-    ///You should return a jsnon like this one
-    ///```json
+    /// This are the parametters for OpenAi-like function call.
+    /// You should return a jsnon like this one
+    /// ```json
     /// {
     ///     "type": "object",
     ///     "properties": {
@@ -36,14 +39,33 @@ pub trait Tool: Send + Sync {
             "required": ["input"]
         })
     }
+
+    /// Processes an input string and executes the tool's functionality, returning a `Result`.
+    ///
+    /// This function utilizes `parse_input` to parse the input and then calls `run`.
+    /// Its used by the Agent
     async fn call(&self, input: &str) -> Result<String, Box<dyn Error>> {
         let input = self.parse_input(input).await;
         self.run(input).await
     }
 
+    /// Executes the core functionality of the tool.
+    ///
+    /// Example implementation:
+    /// ```rust,ignore
+    /// async fn run(&self, input: Value) -> Result<String, Box<dyn Error>> {
+    ///     let input_str = input.as_str().ok_or("Input should be a string")?;
+    ///     self.simple_search(input_str).await
+    /// }
+    /// ```
     async fn run(&self, input: Value) -> Result<String, Box<dyn Error>>;
 
+    /// Parses the input string, which could be a JSON value or a raw string, depending on the LLM model.
+    ///
+    /// Implement this function to extract the parameters needed for your tool. If a simple
+    /// string is sufficient, the default implementation can be used.
     async fn parse_input(&self, input: &str) -> Value {
+        log::info!("Using default implementation: {}", input);
         match serde_json::from_str::<Value>(input) {
             Ok(input) => {
                 if input["input"].is_string() {
