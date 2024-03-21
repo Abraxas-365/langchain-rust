1b00403d0ed76a191820563994ef50c862432e46
Luis Fernando Miranda <63959220+Abraxas-365@users.noreply.github.com>
2024-03-21 10:36:59 -0500

    Feature/question answer with options (#79)
    * refactor: Update OpenAI module with new functionality and structure improvements
    
    * feat: adding from Tool trait to open ai fucntions
    
    * feat(chain): add ChainCallOptions to load_stuff_qa functions
    
    This commit introduces an Optional ChainCallOptions parameter to the load_stuff_qa function in both src/chain/question_answering.rs and src/chain/stuff_documents/chain.rs files to allow setting options while loading question and answering. A new load_stuff_qa_with_options function was also added to the StuffDocument struct to handle the new option.
    
    * feat: streamline LLMChainBuilder in question_answering.rs
    
    Removed the unnecessary mutable declaration on  and consolidated the  assignment and building steps into one. All changes made in .

--- src/tools/tool.rs
+++ src/tools/tool.rs
@@ -2,10 +2,39 @@ use std::error::Error;
 use std::string::String;
 
 use async_trait::async_trait;
+use serde_json::{json, Value};
 
 #[async_trait]
 pub trait Tool: Send + Sync {
     fn name(&self) -> String;
     fn description(&self) -> String;
+    ///This are the parametters for OpenAi like function call.
+    ///You should return a jsnon like this one
+    ///```json
+    /// {
+    ///     "type": "object",
+    ///     "properties": {
+    ///         "command": {
+    ///             "type": "string",
+    ///             "description": "The raw command you want executed"
+    ///                 }
+    ///     },
+    ///     "required": ["command"]
+    /// }
+    ///
+    /// If there s no implementation the defaul will be the self.description()
+    ///```
+    fn parameters(&self) -> Value {
+        json!({
+            "type": "object",
+                "properties": {
+                "command": {
+                    "type": "string",
+                    "description":self.description()
+                }
+            },
+            "required": ["command"]
+        })
+    }
     async fn call(&self, input: &str) -> Result<String, Box<dyn Error>>;
 }
