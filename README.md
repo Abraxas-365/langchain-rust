33a72183bd9b6879fa2b09aeb53863a909ab6e87
Abraxas-365 <luisfmiranda8@gmail.com>
2024-04-07 10:21:23 -0500

    chore: update README and llm.rs files
    Updated README.md to correct the link for Ollama and Compatible Api in LangChain Rust implementation.
    Also, modified llm.rs file to include OpenAIConfig and demonstrate calling Ollama via OpenAI.

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ This is the Rust language implementation of [LangChain](https://github.com/langc
 
   - [x] [OpenAi](https://github.com/Abraxas-365/langchain-rust/blob/main/examples/llm.rs)
   - [x] [Azure OpenAi](https://github.com/Abraxas-365/langchain-rust/blob/main/examples/llm_azure_open_ai.rs)
-  - [x] [Ollama and Compatible Api](https://github.com/Abraxas-365/langchain-rust/blob/main/examples/llm_azure_open_ai.rs)
+  - [x] [Ollama and Compatible Api](https://github.com/Abraxas-365/langchain-rust/blob/main/examples/llm.rs)
   - [x] [Anthropic Claude](https://github.com/Abraxas-365/langchain-rust/blob/main/examples/llm_anthropic_claude.rs)
 
 - Embeddings
--- examples/llm.rs
+++ examples/llm.rs
@@ -1,18 +1,23 @@
+use langchain_rust::llm::OpenAIConfig;
 use langchain_rust::{language_models::llm::LLM, llm::openai::OpenAI};
-//use langchain_rust::llm::OpenAIConfig;
 
 #[tokio::main]
 async fn main() {
-    //You can call Ollama this way
-    // let ollama = OpenAI::default()
-    //     .with_config(
-    //         OpenAIConfig::default()
-    //             .with_api_base("http://localhost:11434/v1")
-    //             .with_api_key("ollama"),
-    //     )
-    //     .with_model("llama2");
-
+    //OpenAI Example
     let open_ai = OpenAI::default();
     let response = open_ai.invoke("hola").await.unwrap();
+    println!("{}", response);
+
+    //Since Ollmama is OpenAi compatible
+    //You can call Ollama this way:
+    let ollama = OpenAI::default()
+        .with_config(
+            OpenAIConfig::default()
+                .with_api_base("http://localhost:11434/v1")
+                .with_api_key("ollama"),
+        )
+        .with_model("llama2");
+
+    let response = ollama.invoke("hola").await.unwrap();
     println!("{}", response);
 }
