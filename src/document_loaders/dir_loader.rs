818cb30e05007cc929b9c2dbf472d97050944e99
scc <66scc66@gmail.com>
2024-04-19 11:54:55 +0800

    feat: add source code loader (#113)

---
+++ src/document_loaders/dir_loader.rs
@@ -0,0 +1,167 @@
+use async_recursion::async_recursion;
+use std::{path::Path, pin::Pin};
+use tokio::fs;
+
+use super::LoaderError;
+
+#[derive(Debug, Clone)]
+pub struct DirLoaderOptions {
+    pub glob: Option<String>,
+    pub suffixes: Option<Vec<String>>,
+    pub exclude: Option<Vec<String>>,
+}
+
+impl Default for DirLoaderOptions {
+    fn default() -> Self {
+        Self {
+            glob: None,
+            suffixes: None,
+            exclude: None,
+        }
+    }
+}
+
+/// Recursively list all files in a directory
+#[async_recursion]
+pub async fn list_files_in_path(
+    dir_path: &Path,
+    files: &mut Vec<String>,
+) -> Result<Pin<Box<()>>, LoaderError> {
+    if dir_path.is_file() {
+        files.push(dir_path.to_string_lossy().to_string());
+        return Ok(Box::pin(()));
+    }
+    if !dir_path.is_dir() {
+        return Err(LoaderError::OtherError(format!(
+            "Path is not a directory: {:?}",
+            dir_path
+        )));
+    }
+    let mut reader = fs::read_dir(dir_path).await.unwrap();
+    while let Some(entry) = reader.next_entry().await.unwrap() {
+        let path = entry.path();
+
+        if path.is_file() {
+            files.push(path.to_string_lossy().to_string());
+        } else if path.is_dir() {
+            list_files_in_path(&path, files).await.unwrap();
+        }
+    }
+    Ok(Box::pin(()))
+}
+
+/// Find files in a directory that match the given options
+pub async fn find_files_with_extension(folder_path: &str, opts: &DirLoaderOptions) -> Vec<String> {
+    let mut matching_files = Vec::new();
+    let folder_path = Path::new(folder_path);
+
+    let mut all_files: Vec<String> = Vec::new();
+    list_files_in_path(&folder_path, &mut all_files)
+        .await
+        .unwrap();
+
+    for file_name in all_files {
+        let path_str = file_name;
+
+        // check if the file has the required extension
+        if let Some(suffixes) = &opts.suffixes {
+            let mut has_suffix = false;
+            for suffix in suffixes {
+                if path_str.ends_with(suffix) {
+                    has_suffix = true;
+                    break;
+                }
+            }
+            if !has_suffix {
+                continue;
+            }
+        }
+
+        if let Some(exclude_list) = &opts.exclude {
+            let mut is_excluded = false;
+            for exclude_pattern in exclude_list {
+                if path_str.contains(exclude_pattern) {
+                    is_excluded = true;
+                    break;
+                }
+            }
+            if is_excluded {
+                continue;
+            }
+        }
+        // check if the file matches the glob pattern
+        if let Some(glob_pattern) = &opts.glob {
+            let glob = glob::Pattern::new(glob_pattern).unwrap();
+            if !glob.matches(&path_str) {
+                continue;
+            }
+        }
+
+        matching_files.push(path_str);
+    }
+
+    matching_files
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env;
+
+    #[tokio::test]
+    async fn test_find_files_with_extension() {
+        // Create a temporary directory for testing
+        let temp_dir = env::temp_dir().join("dir_loader_test_dir");
+
+        if temp_dir.exists() {
+            fs::remove_dir_all(&temp_dir)
+                .await
+                .expect("Failed to remove existing directory");
+        }
+
+        fs::create_dir(&temp_dir)
+            .await
+            .expect("Failed to create temporary directory");
+        // Create some files with different extensions
+        let file_paths = [
+            temp_dir.as_path().join("file1.txt"),
+            temp_dir.as_path().join("file2.txt"),
+            temp_dir.as_path().join("file3.md"),
+            temp_dir.as_path().join("file4.txt"),
+        ];
+
+        // Write some content to the files
+        for path in &file_paths {
+            let content = "Hello, world!";
+            std::fs::write(&path, content).expect("Failed to write file");
+        }
+
+        // Call the function to find files with the ".txt" extension
+        let found_files = find_files_with_extension(
+            temp_dir.as_path().to_str().unwrap(),
+            &DirLoaderOptions {
+                glob: None,
+                suffixes: Some(vec![".txt".to_string()]),
+                exclude: None,
+            },
+        )
+        .await
+        .into_iter()
+        .collect::<Vec<_>>();
+
+        // Expecting to find 3 files with ".txt" extension
+        assert_eq!(found_files.len(), 3);
+        // Expecting each file name to contain ".txt" extension
+        for file in &found_files {
+            assert!(file.ends_with(".txt"));
+        }
+        assert!(found_files.contains(&temp_dir.join("file1.txt").to_string_lossy().to_string()),);
+        assert!(found_files.contains(&temp_dir.join("file2.txt").to_string_lossy().to_string()),);
+        assert!(found_files.contains(&temp_dir.join("file4.txt").to_string_lossy().to_string()),);
+
+        // Clean up: remove the temporary directory and its contents
+        fs::remove_dir_all(&temp_dir)
+            .await
+            .expect("Failed to remove temporary directory");
+    }
+}
---
+++ src/document_loaders/source_code_loader/source_code_loader.rs
@@ -0,0 +1,177 @@
+use crate::document_loaders::{
+    find_files_with_extension, process_doc_stream, DirLoaderOptions, LoaderError,
+};
+use crate::{document_loaders::Loader, schemas::Document, text_splitter::TextSplitter};
+use async_stream::stream;
+use async_trait::async_trait;
+use futures::Stream;
+
+use std::fs::File;
+use std::io::Read;
+use std::pin::Pin;
+
+use super::{get_language_by_filename, LanguageParser, LanguageParserOptions};
+
+#[derive(Debug, Clone)]
+pub struct SourceCodeLoader {
+    file_path: Option<String>,
+    string_input: Option<String>,
+    dir_loader_options: DirLoaderOptions,
+    parser_option: LanguageParserOptions,
+}
+
+impl SourceCodeLoader {
+    pub fn from_string<S: Into<String>>(input: S) -> Self {
+        Self {
+            string_input: Some(input.into()),
+            file_path: None,
+            parser_option: LanguageParserOptions::default(),
+            dir_loader_options: DirLoaderOptions::default(),
+        }
+    }
+}
+
+impl SourceCodeLoader {
+    pub fn from_path<S: Into<String>>(path: S) -> Self {
+        Self {
+            file_path: Some(path.into()),
+            string_input: None,
+            parser_option: LanguageParserOptions::default(),
+            dir_loader_options: DirLoaderOptions::default(),
+        }
+    }
+}
+
+impl SourceCodeLoader {
+    pub fn with_parser_option(mut self, parser_option: LanguageParserOptions) -> Self {
+        self.parser_option = parser_option;
+        self
+    }
+
+    pub fn with_dir_loader_options(mut self, dir_loader_options: DirLoaderOptions) -> Self {
+        self.dir_loader_options = dir_loader_options;
+        self
+    }
+}
+
+#[async_trait]
+impl Loader for SourceCodeLoader {
+    async fn load(
+        mut self,
+    ) -> Result<
+        Pin<Box<dyn Stream<Item = Result<Document, LoaderError>> + Send + 'static>>,
+        LoaderError,
+    > {
+        let string_input = self.string_input.clone();
+        let file_path = self.file_path.clone();
+
+        if let Some(file_path) = file_path {
+            let files =
+                find_files_with_extension(file_path.as_str(), &self.dir_loader_options).await;
+            let stream = stream! {
+                for filename in files {
+                    let mut file = match File::open(&filename) {
+                        Ok(file) => file,
+                        Err(e) => {
+                            yield Err(LoaderError::OtherError(format!("Error opening file: {:?}", e)));
+                            continue;
+                        }
+                    };
+                    let mut content = String::new();
+                file.read_to_string(&mut content).unwrap();
+                    let language = get_language_by_filename(&filename);
+                    let mut parser = LanguageParser::from_language(language).with_parser_option(self.parser_option.clone());
+                    let docs = parser.parse_code(&content);
+                    for doc in docs {
+                        yield Ok(doc);
+                    }
+                }
+            };
+
+            return Ok(Box::pin(stream));
+        } else if let Some(content) = string_input {
+            let language = self.parser_option.language.clone();
+            let stream = stream! {
+                    let mut parser = LanguageParser::from_language(language).with_parser_option(self.parser_option.clone());
+                    let docs = parser.parse_code(&content);
+                    for doc in docs {
+                        yield Ok(doc);
+                    }
+            };
+
+            return Ok(Box::pin(stream));
+        }
+        Err(LoaderError::OtherError(
+            "No file path or string input provided".to_string(),
+        ))
+    }
+
+    async fn load_and_split<TS: TextSplitter + 'static>(
+        mut self,
+        splitter: TS,
+    ) -> Result<
+        Pin<Box<dyn Stream<Item = Result<Document, LoaderError>> + Send + 'static>>,
+        LoaderError,
+    > {
+        let doc_stream = self.load().await?;
+        let stream = process_doc_stream(doc_stream, splitter).await;
+        Ok(Box::pin(stream))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use futures_util::StreamExt;
+
+    use crate::document_loaders::{Language, LanguageContentTypes};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_sourse_code_loader() {
+        let loader_with_dir =
+            SourceCodeLoader::from_path("./src/document_loaders/test_data".to_string())
+                .with_dir_loader_options(DirLoaderOptions {
+                    glob: None,
+                    suffixes: Some(vec!["rs".to_string()]),
+                    exclude: None,
+                });
+
+        let stream = loader_with_dir.load().await.unwrap();
+        let documents = stream.map(|x| x.unwrap()).collect::<Vec<_>>().await;
+
+        assert_eq!(documents.len(), 1);
+        assert_eq!(
+            documents[0].metadata.get("content_type").unwrap(),
+            LanguageContentTypes::SimplifiedCode.to_string().as_str()
+        );
+
+        let loader_with_file =
+            SourceCodeLoader::from_path("./src/document_loaders/test_data/example.rs".to_string());
+        let documents_with_file = loader_with_file
+            .load()
+            .await
+            .unwrap()
+            .map(|x| x.unwrap())
+            .collect::<Vec<_>>()
+            .await;
+        assert_eq!(documents_with_file.len(), documents.len());
+
+        let mut example_file = File::open("./src/document_loaders/test_data/example.rs").unwrap();
+        let mut example_content = String::new();
+        example_file.read_to_string(&mut example_content).unwrap();
+        let loader_with_content = SourceCodeLoader::from_string(example_content)
+            .with_parser_option(LanguageParserOptions {
+                language: Language::Rust,
+                ..Default::default()
+            });
+        let documents_with_content = loader_with_content
+            .load()
+            .await
+            .unwrap()
+            .map(|x| x.unwrap())
+            .collect::<Vec<_>>()
+            .await;
+        assert_eq!(documents_with_content.len(), documents.len());
+    }
+}
