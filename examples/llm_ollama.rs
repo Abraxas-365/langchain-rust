cc031e14fc5d8ce52c10f1d5242a7e5093c246e5
Prabir Shrestha <mail@prabir.me>
2024-05-30 21:56:14 -0700

    add fastemebed under a feature flag

--- examples/llm_ollama.rs
+++ examples/llm_ollama.rs
@@ -1,9 +1,11 @@
+// cargo run --example llm_ollama --features=ollama
+
 use langchain_rust::{language_models::llm::LLM, llm::ollama::client::Ollama};
 
 #[tokio::main]
 async fn main() {
     let ollama = Ollama::default().with_model("llama3");
 
-    let response = ollama.invoke("hola").await.unwrap();
+    let response = ollama.invoke("Hi").await.unwrap();
     println!("{}", response);
 }
