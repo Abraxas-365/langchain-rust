605f58f2cc237548271991594e2db6099281208c
Federico G. Schwindt <fgsch@lodoss.net>
2024-06-17 02:50:48 +0100

    Feature gate these so tests can run

--- examples/embedding_ollama.rs
+++ examples/embedding_ollama.rs
@@ -1,9 +1,9 @@
-// cargo run --example embedding_ollama --features=ollama
-
+#[cfg(feature = "ollama")]
 use langchain_rust::embedding::{
     embedder_trait::Embedder, ollama::ollama_embedder::OllamaEmbedder,
 };
 
+#[cfg(feature = "ollama")]
 #[tokio::main]
 async fn main() {
     let ollama = OllamaEmbedder::default().with_model("nomic-embed-text");
@@ -11,4 +11,11 @@ async fn main() {
     let response = ollama.embed_query("Why is the sky blue?").await.unwrap();
 
     println!("{:?}", response);
+}
+
+#[cfg(not(feature = "ollama"))]
+fn main() {
+    println!("This example requires the 'ollama' feature to be enabled.");
+    println!("Please run the command as follows:");
+    println!("cargo run --example embedding_ollama --features=ollama");
 }
