11325502b20fa850a01c67815bb7f798b02699e2
Prabir Shrestha <mail@prabir.me>
2024-10-12 13:32:59 -0700

    add PlainTextSplitter and text to speech example (#249)

--- src/text_splitter/mod.rs
+++ src/text_splitter/mod.rs
@@ -1,11 +1,13 @@
 mod error;
 mod markdown_splitter;
 mod options;
+mod plain_text_splitter;
 mod text_splitter;
 mod token_splitter;
 
 pub use error::*;
 pub use markdown_splitter::*;
 pub use options::*;
+pub use plain_text_splitter::*;
 pub use text_splitter::*;
 pub use token_splitter::*;
