5094813f651b24faaf5093bf0c1236d917dce8f2
Hiram Chirino <hchirino@redhat.com>
2024-10-14 12:00:22 -0400

    feat(agent): Add agent tools call messages in the agent memory.  Fixes #245 (#246)

--- src/agent/executor.rs
+++ src/agent/executor.rs
@@ -4,6 +4,8 @@ use async_trait::async_trait;
 use serde_json::json;
 use tokio::sync::Mutex;
 
+use super::{agent::Agent, AgentError};
+use crate::schemas::{LogTools, Message};
 use crate::{
     chain::{chain_trait::Chain, ChainError},
     language_models::GenerateResult,
@@ -15,8 +17,6 @@ use crate::{
     },
     tools::Tool,
 };
-
-use super::{agent::Agent, AgentError};
 
 pub struct AgentExecutor<A>
 where
@@ -128,7 +128,25 @@ where
                 AgentEvent::Finish(finish) => {
                     if let Some(memory) = &self.memory {
                         let mut memory = memory.lock().await;
-                        memory.add_user_message(&input_variables["input"]);
+
+                        memory.add_user_message(match &input_variables["input"] {
+                            // This avoids adding extra quotes to the user input in the history.
+                            serde_json::Value::String(s) => s,
+                            x => x, // this the json encoded value.
+                        });
+
+                        let mut tools_ai_message_seen: HashMap<String, ()> = HashMap::default();
+                        for (action, observation) in steps {
+                            let LogTools { tool_id, tools } = serde_json::from_str(&action.log)?;
+                            let tools_value: serde_json::Value = serde_json::from_str(&tools)?;
+                            if tools_ai_message_seen.insert(tools, ()).is_none() {
+                                memory.add_message(
+                                    Message::new_ai_message("").with_tool_calls(tools_value),
+                                );
+                            }
+                            memory.add_message(Message::new_tool_message(observation, tool_id));
+                        }
+
                         memory.add_ai_message(&finish.output);
                     }
                     return Ok(GenerateResult {
